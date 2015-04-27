ad3b0809f865ab9202801f993dd0e67f9169dd3a
YuZakuro <zakuro@yuzakuro.me>
2015-04-27 16:59:24 +0900

    20142014

--- 2014.cpp
+++ 2014.cpp
@@ -1,71 +1,93 @@
 #include<iostream>
 #include<string>
+#include<queue>
 using namespace std;
 
 int W, H;
-string mem[50];
+string m[50];
 int t[50][50];
+int v[10000];
 
-int main()
+int dx[4] = {1, -1, 0, 0}, dy[4] = {0, 0, 1, -1};
+
+int dfs(int xx, int yy, int n)
 {
-  cin >> W >> H;
-  for(int i = 0; i < H; i++)
-    cin >> mem[i];
-  for(int i = 0; i < H; i++)
+  int res = 1;
+  t[yy][xx] = n;
+  queue<int> q;
+  q.push(xx);
+  q.push(yy);
+
+  while(q.size())
   {
-    for(int j = 0; j < W; j++)
+    int x = q.front(); q.pop();
+    int y = q.front(); q.pop();
+
+    if(m[y][x] != '.')
     {
-      if(mem[i][j] != '.')
+      if(v[n] == 0)
       {
-        for(int y = i + 1; y < H; y++)
+        if(m[y][x] == 'W')
+          v[n] = 1;
+        else
+          v[n] = 2;
+      }
+      if(v[n] == 1 && m[y][x] == 'B' || v[n] == 2 && m[y][x] == 'W')
+        v[n] = 3;
+      continue;
+    }
+
+    for(int i = 0; i < 4; i++)
+    {
+      int tx = x + dx[i], ty = y + dy[i];
+      if(0 <= tx && tx < W && 0 <= ty && ty < H && t[ty][tx] <= 0)
+      {
+        q.push(tx);
+        q.push(ty);
+        if(!t[ty][tx])
         {
-          if(mem[y][j] != '.')
-            break;
-          if(t[j][y] == 0 || t[j][y] == 'W' ? 1 : -1)
-            t[j][y] = mem[i][j] == 'W' ? 1 : -1;
-          else
-            t[j][y] = 10;
-        }
-        for(int y = i - 1; y >= 0; y--)
-        {
-          if(mem[y][j] != '.')
-            break;
-          if(t[j][y] == 0 || t[j][y] == 'W' ? 1 : -1)
-            t[j][y] = mem[i][j] == 'W' ? 1 : -1;
-          else
-            t[j][y] = 10;
-        }
-        for(int x = j + 1; x < W; x++)
-        {
-          if(mem[i][x] != '.')
-            break;
-          if(t[x][i] == 0 || t[x][i] == 'W' ? 1 : -1)
-            t[x][i] = mem[i][j] == 'W' ? 1 : -1;
-          else
-            t[x][i] = 10;
-        }
-        for(int x = j - 1; x >= 0; x--)
-        {
-          if(mem[i][x] != '.')
-            break;
-          if(t[x][i] == 0 || t[x][i] == 'W' ? 1 : -1)
-            t[x][i] = mem[i][j] == 'W' ? 1 : -1;
-          else
-            t[x][i] = 10;
+          res++;
+          t[ty][tx] = n;
         }
       }
     }
   }
-  int b = 0, w = 0;
-  for(int i = 0; i < H; i++)
+  return res;
+}
+
+int main()
+{
+  while(true)
   {
-    for(int j = 0; j < W; j++)
+    cin >> W >> H;
+    if(!W)
+      break;
+    fill((int*)t, (int*)t + 2500, 0);
+    fill(v, v + 10000, 0);
+    for(int i = 0; i < H; i++)
     {
-      b += t[j][i] == -1;
-      b += t[j][i] == 1;
-      cout << t[j][i];
+      cin >> m[i];
+      for(int j = 0; j < W; j++)
+        if(m[i][j] != '.')
+          t[i][j] = -1;
     }
-    cout << endl;
+
+    int a = 11, w = 0, b = 0;
+    for(int y = 0; y < H; y++)
+    {
+      for(int x = 0; x < W; x++)
+      {
+        if(t[y][x] == 0)
+        {
+          int r = dfs(x, y, a);
+          if(v[a] == 1)
+            w += r;
+          if(v[a] == 2)
+            b += r;
+          a++;
+        }
+      }
+    }
+    cout << b << " " << w << endl;
   }
-  cout << b << " " << w << endl;
 }
